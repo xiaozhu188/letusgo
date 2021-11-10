3807ad577295fb7ea10fada169207b78bcfb920c
xiaozhu <809057480@qq.com>
2021-11-10 16:05:28 +0800

    feat(views, router): 新增Foo,Bar组件

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <router-link to="/bar">Bar</router-link>
+      <router-link to="/foo">Foo</router-link>
     </div>
     <router-view />
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -13,11 +13,18 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/foo",
+    name: "Foo",
+    component: () => import(/* webpackChunkName: "foo" */ "../views/Foo.vue"),
+  },
+  {
+    path: "/bar",
+    name: "Bar",
+    component: () => import(/* webpackChunkName: "bar" */ "../views/Bar.vue"),
   },
 ];
 
